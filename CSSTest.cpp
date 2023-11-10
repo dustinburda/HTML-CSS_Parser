e8fa66184205c37dae7ef0e438751397fcdc4b8b
dustinburda <dustin99wii@gmail.com>
2023-11-10 13:34:00 -0500

    Finished CSSTests

--- CSSTest.cpp
+++ CSSTest.cpp
@@ -29,5 +29,28 @@ TEST(CSSTest, SelectorConstructorTest) {
 }
 
 TEST(CSSTest, DeclarationConstructorTest) {
+    // margin: auto
+    Declaration d1 {"margin", "auto"};
+    EXPECT_EQ(d1.value_type_, Value_Type::Keyword);
+    EXPECT_EQ(d1.name_, "margin");
+    EXPECT_EQ(std::get<std::string>(d1.var_value_), "auto");
+    EXPECT_EQ(to_px(d1), 0);
 
+    // margin-bottom: 10px
+    Declaration d2 {"margin-bottom", {10, Unit::Pixel}};
+    EXPECT_EQ(d2.value_type_, Value_Type::Length);
+    EXPECT_EQ(d2.name_, "margin-bottom");
+    auto true_val2 = std::get<std::tuple<int, Unit>>(d2.var_value_);
+    std::tuple<int, Unit> expected_val2 = std::tuple<int, Unit>(10, Unit::Pixel);
+    EXPECT_EQ(true_val2, expected_val2);
+    EXPECT_EQ(to_px(d2), 10);
+
+    // color: FF0000
+    Declaration d3 {"color", Color {255, 0, 0, 255} };
+    EXPECT_EQ(d3.value_type_, Value_Type::ColorValue);
+    EXPECT_EQ(d3.name_, "color");
+    auto true_val3 = std::get<Color>(d3.var_value_);
+    Color expected_val3 = Color {255, 0, 0, 255};
+    EXPECT_EQ(true_val3, expected_val3);
+    EXPECT_EQ(to_px(d3), 0);
 }
